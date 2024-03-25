e8b7cfefb5f3ee427830c830cfa309e52c83ca4a
kaanboyacii <kaanboyacibn@gmail.com>
2024-03-25 16:18:27 +0300

    Add project files.

---
+++ LogAnalyst/LogParser.cs
@@ -0,0 +1,30 @@
+﻿using Sprache;
+using System.Linq;
+
+namespace LogAnalyst
+{
+    public static class LogParser
+    {
+        public static LogEntry ParseLog(string log)
+        {
+            var logLevel = Parse.String("[ERR]").Or(Parse.String("[WRN]")).Or(Parse.String("[INF]")).Text().Token().Optional();
+            var logEntryParser =
+                from source in Parse.AnyChar.Except(Parse.String(" "))
+                    .Many().Text().Token()
+                from date in Parse.CharExcept(' ').Many().Text().Token()
+                from time in Parse.CharExcept(' ').Many().Text().Token()
+                from level in logLevel
+                from message in Parse.AnyChar.Many().Text()
+                select new LogEntry
+                {
+                    Source = source,
+                    Date = date,
+                    Time = time,
+                    Level = level.GetOrElse(""),
+                    Message = message
+                };
+
+            return logEntryParser.Parse(log);
+        }
+    }
+}
---
+++ LogAnalyst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyst
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            LogAnalyst myService = new LogAnalyst();
+            myService.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new LogAnalyst()
+            //};
+            //ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
